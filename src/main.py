41c4159ac398089c6656a21bf382417bb70d236a
chickwn <chickwn@gmail.com>
2025-02-26 20:54:00 +0000

    - fix get_statistics bug: use forUsername instead of id for channel lookup - added comments in youtube_api.py and few prints for debugging purposes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
+from youtube_api import YouTubeAPI
 
 def main():
     load_dotenv()
     API_KEY = os.getenv("API_KEY")
     CHANNEL_HANDLE = "GoogleDevelopers"
 
+    a = YouTubeAPI()
+    subscriber_count = a.get_statistics(CHANNEL_HANDLE, API_KEY)
+    print("Subscriber Count:", subscriber_count)
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     main()
--- src/youtube_api.py
+++ src/youtube_api.py
@@ -7,14 +7,24 @@ class YouTubeAPI:
 
     def get_statistics(self, channel_handle: str, api_key: str):
 
+        """
+        Fetches the subscriber count for a give YouTube channel
+        :param channel_handle: The unique identifier of the YouTube channel
+        :param api_key: Your YouTube v3 API key
+        :return: The subscriber count
+        """
+
+        # Define the API endpoint for fetching channel data
         url = 'https://www.googleapis.com/youtube/v3/channels'
 
+        # Set up the parameters for the API request
         params = {
             "part": "statistics",
-            "id": channel_handle,
+            "forUsername": channel_handle,
             "key": api_key
         }
 
+        # Attempt to make the API call.
         try:
             response = requests.get(url, params=params)
 
@@ -24,15 +34,24 @@ class YouTubeAPI:
 
         # Check if the request was successful
         if response.status_code == 200:
+            print("HTTP Request Successful")
             try:
+                #parse the JSON response
                 data = response.json()
-
+                print(data)
             except Exception as e:
-                print(f"Error:{str(e)}")
+                # Handle JSON parsing errors
+                print(f"Error parsing JSON: {str(e)}")
                 return None
+
+            # Check if 'data' is not empty
             if data:
                 return data["items"][0]["statistics"]["subscriberCount"]
             else:
+                # Print an error if no data is returned
                 print("Error: Empty data")
+                return None
         else:
+            # If the API returns an error status, print out the error detials
             print("Error:", response.status_code, response.text)
+            return None
