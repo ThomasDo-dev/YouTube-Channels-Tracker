a3e82120be8d892c8ed4a5b9e19c9e1e7526a64c
chickwn <chickwn@gmail.com>
2025-02-26 20:29:19 +0000

    - created a class YouTubeAPI in youtube_api.py   - created a method get_statistics to return Sub counts of a channel

--- src/main.py
+++ src/main.py
@@ -1,18 +1,11 @@
-import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-CHANNEL_ID = "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+def main():
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    CHANNEL_HANDLE = "GoogleDevelopers"
 
-url = 'https://www.googleapis.com/youtube/v3/channels'
-params = {
-    "part": "snippet,statistics",
-    "id": CHANNEL_ID,
-    "key": API_KEY
-}
 
-response = requests.get(url, params=params)
-data = response.json()
-print(data)
+if __name__ == "main":
+    main()
--- src/youtube_api.py
+++ src/youtube_api.py
@@ -1,4 +1,38 @@
 import requests
 
-class YouTubeAPI():
+class YouTubeAPI:
+
     def __init__(self):
+        pass
+
+    def get_statistics(self, channel_handle: str, api_key: str):
+
+        url = 'https://www.googleapis.com/youtube/v3/channels'
+
+        params = {
+            "part": "statistics",
+            "id": channel_handle,
+            "key": api_key
+        }
+
+        try:
+            response = requests.get(url, params=params)
+
+        except Exception as e:
+            print(f"Error:{str(e)}")
+            return None
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            try:
+                data = response.json()
+
+            except Exception as e:
+                print(f"Error:{str(e)}")
+                return None
+            if data:
+                return data["items"][0]["statistics"]["subscriberCount"]
+            else:
+                print("Error: Empty data")
+        else:
+            print("Error:", response.status_code, response.text)
