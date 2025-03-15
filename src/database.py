b81dfbb529c23cc0fd76221f76a7ceec0e9807c8
chickwn <chickwn@gmail.com>
2025-03-15 23:58:18 +0000

    Initial Thoughts on how to design a database to suit the data analysis and automation down the line

---
+++ src/database.py
@@ -0,0 +1,16 @@
+"""
+What's the right database?
+
+1.Choose which type of database
+- SQL vs NoSQl?
+sql - querying capabilities -> if this route need to design database schema or
+-> there is relational databses that support JSON data types (like PostgreSQL)
+nosql - unstructred storage like JSON
+
+2. How and what to ingest the data into the database
+-> make sure process to handle erroe and logs for monitoring purporses
+
+Things to keep in mind what data analysis I want to do. In this case
+want to analyse trends over timehence including timestap fields to track when data was fetched
+
+"""
